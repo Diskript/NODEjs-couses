7e3bba7a7c8c1fd946d9c6a68a475d94f40a3d3b
Diskript <i22901ib@gmail.com>
2026-01-03 16:52:46 +0300

    3 and 4 tasks completed

--- JS-TS/solutions/todo-crud.ts
+++ JS-TS/solutions/todo-crud.ts
@@ -1,17 +1,49 @@
-import { Todo } from './types';
+import { Todo } from "./types";
 
 export function addTodo(state: Todo[], todo: Todo): Todo[] {
-  throw new Error('addTodo: not implemented');
+  const newTodoList = [...state];
+  newTodoList.push(todo);
+  return newTodoList;
 }
 
-export function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Todo[] {
-  throw new Error('updateTodo: not implemented');
+export function updateTodo(
+  state: Todo[],
+  id: number,
+  update: Partial<Omit<Todo, "id" | "createdAt">>,
+): Todo[] {
+  if (state == null) throw new Error("updateTodo: state is undefined or null");
+  if (state.length === 0) throw new Error("updateTodo: state is empty");
+
+  for (let todo of state) {
+    if (todo.id === id) {
+      const updatedTodo = { ...todo, ...update };
+      const newState = [...state];
+      const index = newState.findIndex((t) => t.id === id);
+      newState[index] = updatedTodo;
+      return newState;
+    }
+  }
+  throw new Error("updateTodo: todo not found");
 }
 
 export function removeTodo(state: Todo[], id: number): Todo[] {
-  throw new Error('removeTodo: not implemented');
+  if (state == null) throw new Error("removeTodo: state is undefined or null");
+  if (state.length === 0) throw new Error("removeTodo: state is empty");
+  for (let todo of state) {
+    if (todo.id === id) {
+      const newTodoList = [...state];
+      newTodoList.splice(state.indexOf(todo), 1);
+      return newTodoList;
+    }
+  }
+  throw new Error("removeTodo: todo id not found");
 }
 
 export function getTodo(state: Todo[], id: number): Todo | undefined {
-  throw new Error('getTodo: not implemented');
+  for (let todo of state) {
+    if (todo.id === id) {
+      return todo;
+    }
+  }
+  throw new Error("getTodo: todo id not found");
 }
--- JS-TS/solutions/todo-factory.ts
+++ JS-TS/solutions/todo-factory.ts
@@ -1,7 +1,14 @@
-import { Todo, NewTodo, TodoStatus } from './types';
+import { Todo, NewTodo, TodoStatus } from "./types";
 
 let nextId = 1;
 
 export function createTodo(input: NewTodo): Todo {
-  throw new Error('createTodo: not implemented');
+  const newTodo: Todo = {
+    id: nextId++,
+    title: input.title,
+    descriptin: input.descriptin,
+    status: input.status || TodoStatus.PENDING,
+    createdAt: new Date(),
+  };
+  return newTodo;
 }
